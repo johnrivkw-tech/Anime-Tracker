67e0c5eecc4952f30ee78eb349ee7a0981e816a7
johnrivkw-tech <johnrivkw@gmail.com>
2026-07-04 10:15:07 -0700

    Increment database version from 3 to 4

--- app/src/main/java/com/example/animetracker/data/AnimeDatabase.kt
+++ app/src/main/java/com/example/animetracker/data/AnimeDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Anime::class], version = 3, exportSchema = false)
+@Database(entities = [Anime::class], version = 4, ok exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AnimeDatabase : RoomDatabase() {
 
